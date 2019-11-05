872bd0e267ae84142f9bdc2476e01eca5afdb4b9
breadbread1984 <breadbread1984@163.com>
2019-11-05 10:18:10 +0800

    add moment measurement

--- preprocess.py
+++ preprocess.py
@@ -2,27 +2,73 @@
 
 import sys;
 import numpy as np;
+import tensorflow as tf;
 import cv2;
 
 def intersection(a,b):
 
-    x = max(a[0], b[0])
-    y = max(a[1], b[1])
-    w = min(a[0]+a[2], b[0]+b[2]) - x
-    h = min(a[1]+a[3], b[1]+b[3]) - y
+    x = max(a[0], b[0]);
+    y = max(a[1], b[1]);
+    w = min(a[0]+a[2], b[0]+b[2]) - x;
+    h = min(a[1]+a[3], b[1]+b[3]) - y;
     if w < 0 or h < 0: return (0,0,0,0);
     return (x, y, w, h);
 
 def union(a,b):
 
-    x = min(a[0], b[0])
-    y = min(a[1], b[1])
-    w = max(a[0]+a[2], b[0]+b[2]) - x
-    h = max(a[1]+a[3], b[1]+b[3]) - y
-    return (x, y, w, h)
+    x = min(a[0], b[0]);
+    y = min(a[1], b[1]);
+    w = max(a[0]+a[2], b[0]+b[2]) - x;
+    h = max(a[1]+a[3], b[1]+b[3]) - y;
+    return (x, y, w, h);
+
+def mean(pts, img = None, weighted = False):
+    
+    assert type(pts) is np.ndarray and len(pts.shape) == 2 and pts.shape[1] == 2;
+    if weighted == True: assert img is not None;
+    if weighted:
+        img = tf.cast(img, dtype = tf.float32);
+        weights = tf.gather_nd(img, tf.reverse(pts, axis = [1]));
+        weights = weights / tf.math.reduce_sum(weights);
+        pts = tf.constant(pts, dtype = tf.float32) * tf.expand_dims(weights, axis = 1);
+        mean = tf.math.reduce_sum(pts, axis = 0);
+    else:
+        pts = tf.constant(pts, dtype = tf.float32);
+        mean = tf.math.reduce_mean(pts, axis = 0);
+    return mean.numpy();
+
+def covariance(pts, m = None, img = None, weighted = False):
+    
+    assert type(pts) is np.ndarray and len(pts.shape) == 2 and pts.shape[1] == 2;
+    if weighted == True: assert img is not None;
+    x = tf.cast(pts, dtype = tf.float32);
+    if m is None:
+        # centralized.shape = (n, 2)
+        centralized = x - mean(x);
+    else:
+        centralized = x - m;
+    if weighted:
+        img = tf.cast(img, dtype = tf.float32);
+        weights = tf.gather_nd(img, tf.reverse(pts, axis = [1]));
+        weights = weights / tf.math.reduce_sum(weights);
+        diag = tf.linalg.diag(weights);
+        cov = tf.linalg.matmul(tf.linalg.matmul(tf.transpose(centralized, (1,0)), diag), centralized);
+        cov = cov / (1 - tf.math.reduce_sum(tf.math.pow(weights,2)));
+    else:
+        # cov.shape = (2,2)
+        cov = tf.linalg.matmul(tf.transpose(centralized, (1, 0)), centralized) / (pts.shape[0] - 1);
+    return cov.numpy();
+    
+def eigvec(cov):
+    
+    assert type(cov) is np.ndarray and len(cov.shape) == 2 and cov.shape[1] == 2;
+    cov = tf.constant(cov, dtype = tf.float32);
+    e,v = tf.linalg.eigh(cov);
+    return e.numpy(), v.numpy();
 
 def preprocess(image):
     
+    # 1) find a foot print instance (bounding, pixels)
     ret, mask = cv2.threshold(image, 1,255,cv2.THRESH_BINARY);
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S);
     comp_boundings = dict();
@@ -55,6 +101,25 @@ def preprocess(image):
             ul = np.array(comp[1][0:2]);
             dr = np.array(comp[1][0:2]) + np.array(comp[1][2:4]);
             cv2.rectangle(image, tuple(ul),tuple(dr), (255,255,255), 2);
+    
+    # 2) calculate mean and eigvec of each foot print
+    for key, foot in comp_boundings.items():
+        pts = foot[0];
+        bounding = foot[1];
+        # center.shape = (2,)
+        center = mean(pts, image, True);
+        # cov.shape = (2,2)
+        cov = covariance(pts, center, image, True);
+        e, v = eigvec(cov);
+        if True:
+            # draw center of the foot
+            cv2.circle(image, tuple(center.astype('int32')), 5, (255,255,255));
+            # mean direction
+            pts1 = center + 0.4 * e[0] * v[:,0];
+            pts2 = center + 0.4 * e[1] * v[:,1];
+            cv2.line(image, tuple(center.astype('int32')), tuple(pts1.astype('int32')), (255,255,255), 1);
+            cv2.line(image, tuple(center.astype('int32')), tuple(pts2.astype('int32')), (255,255,255), 1);
+        
     cv2.imshow('comp', image);
     cv2.waitKey();
 
