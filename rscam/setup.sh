1fa64503cc356f2edce0b31e0358838895e1b008
root <root@LAPTOP-A2EG90UI.localdomain>
2020-10-07 09:23:19 +0800

    update

---
+++ rscam/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rabbitmqctl add_vhost /PlantarPressure
+rabbitmqctl add_user PlantarPressure pcadmin-01
+rabbitmqctl set_permissions -p /PlantarPressure PlantarPressure ".*" ".*" ".*"
+
