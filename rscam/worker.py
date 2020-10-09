3aebef3f6f537375a88516343459c46faaf5f076
breadbread1984 <breadbread1984@163.com>
2020-10-09 18:37:06 +0800

    update

--- rscam/worker.py
+++ rscam/worker.py
@@ -5,6 +5,7 @@ from celery.signals import after_setup_logger;
 import logging;
 import pyrealsense2 as rs;
 import numpy as np;
+import cv2;
 from settings import *;
 
 celery = Celery('worker', backend = MESSAGE_QUEUE_URI, broker = MESSAGE_QUEUE_URI);
@@ -94,7 +95,7 @@ class PlantarPressureWorker(Task):
       pipeline.stop();
       return False, None, None;
   
-  def point_cloud(self, cam_id):
+  def get_point_cloud(self, cam_id):
       
     pipeline = rs.pipeline();
     pipeline.start(self.configs[cam_id]);
@@ -121,6 +122,80 @@ class PlantarPressureWorker(Task):
       verts = np.asanyarray(v).view(np.float32).reshape(-1, 3); # xyz
       texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2); # uv
       # TODO
+
+  def pointcloud(self, out, verts, texcoords, color, painter = True):
+      
+    # TODO
+
+  def project(self, out, v):
+
+    h, w = out.shape[:2];
+    view_aspect = float(h) / w;
+    with np.errstate(divide = 'ignore', invalid = 'ignore'):
+      proj = v[:, :-1] / v[:, -1, np.newaxis] * (w * view_aspect, h) + (w/2.0, h/2.0);
+    znear = 0.03;
+    proj[v[:, 2] < znear] = np.nan;
+    return proj;
+
+  def line3d(self, out, pt1, pt2, color = (0x80, 0x80, 0x80), thickness = 1):
+
+    p0 = self.project(out, p1.reshape(-1, 3))[0];
+    p1 = self.project(out, p2.reshape(-1, 3))[0];
+    if np.isnan(p0).any() or np.isnan(p1).any(): return;
+    p0 = tuple(p0.astype(int));
+    p1 = tuple(p1.astype(int));
+    rect = (0, 0, out.shape[1], out.shape[0]);
+    inside, p0, p1 = cv2.clipline(rect, p0, p1);
+    if inside:
+      cv2.line(out, p0, p1, color, thickness, cv2.LINE_AA);
+
+  def view(self, v, translation = np.array([0, 0, -1], dtype = np.float32), pitch = 0, yaw = 0, distance = 2):
+
+    # input: v original location of the object
+    # output: the location of the object being observed in the camera coordinate system
+    pivot = translation + np.array((0, 0, distance), dtype = np.float32); # location of the rotation center
+    Rx, _ = cv2.Rodrigues((pitch, 0, 0)); # euler angles -> rotation matrix
+    Ry, _ = cv2.Rodrigues((0, yaw, 0)); # euler angles -> rotation matrix
+    rotation = np.dot(Ry, Rx).astype(np.float32); # merged rotation matrix
+    return np.dot(v - pivot, rotation) + pivot - translation; # rotate target object
+
+  def grid(self, out, pos, rotation = np.eye(3), size = 1, n = 10, color = (0x80, 0x80, 0x80)):
+
+    pos = np.array(pos);
+    s = size / float(n);
+    s2 = 0.5 * size;
+    for i in range(0, n+1):
+      x = -s2 + i * s;
+      self.line3d(out, self.view(pos + np.dot((x, 0, -s2), rotation)), self.view(pos + np.dot((x, 0, s2), rotation)), color);
+    for i in range(0, n+1):
+      z = -s2 + i * s;
+      self.line3d(out, self.view(pos + np.dot((-s2, 0, z), rotation)), self.view(pos + np.dot((s2, 0, z), rotation)), color);
+
+  def frustum(self, out, intrinsics, color = (0x40, 0x40, 0x40)):
+
+    orig = self.view([0, 0, 0]);
+    w, h = intrinsics.width, intrinsics.height;
+    for d in range(1, 6, 2):
+      def get_point(x, y):
+        p = rs.rs2_deproject_pixel_to_point(intrinsics, [x, y], d);
+        self.line3d(out, orig, self.view(p), color);
+        return p;
+
+      top_left = get_point(0, 0);
+      top_right = get_point(w, 0);
+      bottom_right = get_point(w, h);
+      bottom_left = get_point(0, h);
+      
+      self.line3d(out, self.view(top_left), self.view(top_right), color);
+      self.line3d(out, self.view(top_right), self.view(bottom_right), color);
+      self.line3d(out, self.view(bottom_right), self.view(bottom_left), color);
+      self.line3d(out, self.view(bottom_left), self.view(top_left), color);
+
+  def axes(self, out, pos, rotation = np.eye(3), size = 0.075, thickness = 2):
+
+    self.line3d(out, pos, pos + np.dot((0, 0, size), rotation), (0xff, 0, 0), thickness);
+    self.line3d(out, pos, pos + np.dot((0, size, 0), rotation), (0, 0xff, 0), thickness);
+    self.line3d(out, pos, pos + np.dot((size, 0, 0), rotation), (0, 0, 0xff), thickness);
 
 @celery.task(name = 'info', base = PlantarPressureWorker)
 def info():
